cc02216c571589ce072bcf61442fe3f268e4a4c4
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-03 09:55:00 -0800

    Fixed errors

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*main - does something
+ *put - does something else
+ *Return - also does something
+ */
+
+int main(void)
+{
+	put "Programming is like building a multilingual puzzle";
+	return (0);
+}
