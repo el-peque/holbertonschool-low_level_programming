e561edd53a40f0967d6bbeb8e228a8aac1f35e86
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-03 13:17:06 -0800

    Added printf program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - sets it up
+ * printf - prints input
+ * Return: returns the outcome
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
