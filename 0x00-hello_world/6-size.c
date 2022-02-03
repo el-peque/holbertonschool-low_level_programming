407d79ab494e8d8e320e3ae274c266ff96bbe400
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-03 15:55:28 -0800

    Added program

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - sets it up
+ * printf - prints input
+ * Return: returns the outcome
+ */
+int main(void)
+{
+	printf("Size of a char:" CHAR_BYTES "bytes");
+
+	return (0);
+}
