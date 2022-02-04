eb4360cc519b91edc33d5a96c70218724844668e
cabreraezequiel <98030236+cabreraezequiel@users.noreply.github.com>
2022-02-04 14:02:32 -0300

    Fixed error

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,15 @@ int main(void)
 	char(ch);
 
         ch='a';
-	while ((ch != 'q' && ch != 'e') && ch <= 'z')
+	while (ch <= 'z')
 {
+	if (ch != 'q' && ch != 'e')
+	{
 	putchar (ch);
 	++ch;
+	else
+	++ch;
+}
 }
 	return(0);
 
