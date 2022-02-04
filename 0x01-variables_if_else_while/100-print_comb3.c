eb751a888f930189601088c4348c6274107e2c5a
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-04 11:50:38 -0800

    Fixed error

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,8 @@ int main(void)
   for(ch = '0' ; ch <= '9' ; ch++)
 	{
 	putchar (ch);
-	if (ch != '9')
-	  {
 	putchar (',');
 	putchar (' ');
-	  }
 	}      
       putchar('\n');
   return (0);
