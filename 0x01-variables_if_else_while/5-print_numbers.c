fef0b49e86666cde2c671e14c4b157f0260f58ff
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-04 09:38:41 -0800

    Added new program

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * while - goes til 10
+ *
+ *
+ * Return: should print 0
+*/
+int main(void)
+{
+	char(num);
+
+	num = 0;
+	while (num < 10)
+{
+	putchar (num);
+	++num;
+}
+	putchar ('\n');
+	return (0);
+
+}
