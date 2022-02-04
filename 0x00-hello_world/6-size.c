b004654765b55c097a1d1b33e0a5bd830fa949f6
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-03 16:31:41 -0800

    Fixed error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,11 +3,16 @@
 /**
  * main - sets it up
  * printf - prints input
+ * sizeof - explanatory
  * Return: returns the outcome
  */
 int main(void)
 {
 	printf("Size of a char: %zu bytes\n", sizeof(char));
+	printf("Size of a int: %zu bytes\n", sizeof(int));
+	printf("Size of a long int: %zu bytes\n", sizeof(long int));
+	printf("Size of a long long int: %zu bytes\n", sizeof(long long int));
+	printf("Size of a float: %zu bytes\n", sizeof(float));
 
 	return (0);
 }
