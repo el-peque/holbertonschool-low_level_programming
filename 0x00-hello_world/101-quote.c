39eeb8c9e308c64c36e6d975e19d5bf5da1bad04
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-03 16:48:29 -0800

    Added optional steps

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", stderr );
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
 
 	return (1);
 }
