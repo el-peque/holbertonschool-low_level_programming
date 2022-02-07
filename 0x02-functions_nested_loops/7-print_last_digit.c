7dd439becc786b40131aeef3a196e6e74ef0c51b
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-07 11:54:51 -0800

    Fixed errors

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,16 +9,17 @@
 int print_last_digit(int n)
 {
 	int lastDigit = n % 10;
-	if (lastDigit >=0)
+
+	if (lastDigit >= 0)
 	{
 		_putchar(lastDigit + '0');
 
-		return(lastDigit);
+		return (lastDigit);
 	}
 	else
 	{
 		_putchar(-lastDigit + '0');
 
-		return(-lastDigit);
+		return (-lastDigit);
 	}
 }
