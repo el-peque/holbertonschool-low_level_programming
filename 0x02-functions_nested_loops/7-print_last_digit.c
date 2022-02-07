ed6392703256060fb56662ee2b3d0ddb4f763203
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-07 11:00:07 -0800

    Added program

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _print_last_digit - computes the absolute value of an integrer
+ * @nem: input character
+ *
+ * Return: 1 if its lowercase, 0 otherwise
+ */
+int print_last_digit(int)
+{
+	int;
+
+	if (int > 0)
+	{
+		return (int);
+	}
+	else if (int < 0)
+	{
+		return (int**(-1));
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _print_last_digit - computes the absolute value of an integrer
+ * @nem: input character
+ *
+ * Return: 1 if its lowercase, 0 otherwise
+ */
+void jack_bauer(void);
+{
+	int hours = '00';
+	int minutes = '00';
+
+	while (hours < '24')
+	{
+		for (minutes = '00' ; minutes <= '59' ; minutes++)
+		{
+			_putchar (hours);
+			_putchar (':');
+			_putchar (minutes);
+		}
+	}
+}
