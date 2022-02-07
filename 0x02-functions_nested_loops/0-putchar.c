5b806656f59b6bde4b54382c474fb8654393efcc
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-07 07:00:58 -0800

    Added new files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "_putchar.c"
+
+int main(void)
+{
+	char ptchr[8] = "_putchar";
+
+	for (char ptchr = 0 ; char ptchr <= 8 ; char ptchr++)
+	{
+		_putchar(char ptchr);
+	}
+	_putchar("\n")
+	return(0)
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "_putchar.c"
+
+void print_alphabet(void);
+int main(void)
+{
+	for (alphbt = 'a' ; alphbt <= 'z' ; alphbt++)
+	{
+		_putchar (aphbt);
+	}
+	_putchar ("\n");
+
+	return(0);
+}
