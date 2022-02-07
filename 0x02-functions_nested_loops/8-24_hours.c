eb910c507975d855b992d74d6da0819b608f6967
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-07 11:07:24 -0800

    Added program

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,12 +8,12 @@
  */
 void jack_bauer(void)
 {
-	int hours [2] = "00";
-        int minutes [2] = "00";
+	int hours [2] = 00;
+        int minutes [2] = 00;
 
-	while (hours < "24")
+	while (hours < 24)
 	{
-		for (minutes = "00" ; minutes <= "59" ; minutes++)
+		for (minutes = 00 ; minutes <= 59 ; minutes++)
 		{
 			_putchar (hours);
 			_putchar (':');
