72ce6dda1e1e89d529f44dfb2b2e1884d730fbcf
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-07 08:55:23 -0800

    Added files

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower(int c) - checks for lowercase character
+ *
+ */
+int _islower(int c)
+{
+	if ('a' <= c <= 'z')
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
