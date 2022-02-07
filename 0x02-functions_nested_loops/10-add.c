2b1f7abca8c2a03dde558be6083e1ecffe46cb75
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-07 15:43:42 -0800

    Fixed program

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -8,20 +8,21 @@
  */
 void times_table(void)
 {
-	char line = '0';
-	char row = '0';
-	char rslt = (row * line);
+	int line = 0;
+	int row = 0;
+	int rslt = 0;
 
-	for (row = '0' ; row <= '9' ; row++)
-		for (line = '0' ; line <= '9' ; line++)
+	for (row = 0 ; row <= 9 ; row++)
+		for (line = 0 ; line <= 9 ; line++)
 		{
+			int rslt = (row * line);
 			if(line != 0)
 			{
 				_putchar(' ');
-			}
-			if(rslt < 10)
-			{
-				_putchar(' ');
+				if(rslt < 10)
+				{
+					_putchar(' ');
+				}
 			}
 			if(rslt > 9)
 			{
@@ -32,7 +33,7 @@ void times_table(void)
 			{
 				_putchar(rslt + '0');
 			}
-			if(line == '9')
+			if(line == 9)
 			{
 				_putchar('\n');
 			}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,14 +8,38 @@
  */
 void times_table(void)
 {
-	char line = '0';
-	char row = '0';
-	char rslt = (row * line);
+	int line = 0;
+	int row = 0;
+	int res = 0;
 
-	for (row = '0' ; row <= '9' ; row++)
-	{
-		_putchar(rslt);
-		_putchar(',');
-		_putchar(' ');
-	}
+	for (row = 0 ; row <= 9 ; row++)
+		for (line = 0 ; line <= 9 ; line++)
+		{
+			res = (row * line);
+			if(line != 0)
+			{
+				_putchar(' ');
+				if(res < 10)
+				{
+					_putchar(' ');
+				}
+			}
+			if(res > 9)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else
+			{
+				_putchar(res + '0');
+			}
+			if(line == 9)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+			}
+		}
 }
