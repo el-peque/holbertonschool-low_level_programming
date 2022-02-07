dbf2203e0fbcbeb0bf0a4ade27ec68b2ce713e5d
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-07 08:27:30 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 = Prints 10 times the alphabet
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char num = 0;
+	char alphaten = 'a';
+
+	for (num = 0 ; num <= 9 ; num++)
+	{
+		for (alphaten = 'a' ; alphaten <= 'z' ; alphaten++)
+		{
+			_putchar(alphaten);
+		}
+		_putchar('\n');
+	}
+}
