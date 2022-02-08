b953beb650f6a6b87a5f2c4993a5de02aa62c9b9
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-07 16:05:26 -0800

    Added program

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,40 +6,8 @@
  *
  * Return: 1 if its lowercase, 0 otherwise
  */
-void times_table(void)
+int add(int a, int b)
 {
-	int line = 0;
-	int row = 0;
-	int rslt = 0;
+	return(a + b)
 
-	for (row = 0 ; row <= 9 ; row++)
-		for (line = 0 ; line <= 9 ; line++)
-		{
-			int rslt = (row * line);
-			if(line != 0)
-			{
-				_putchar(' ');
-				if(rslt < 10)
-				{
-					_putchar(' ');
-				}
-			}
-			if(rslt > 9)
-			{
-				_putchar((rslt / 10) + '0');
-				_putchar((rslt % 10) + '0');
-			}
-			else
-			{
-				_putchar(rslt + '0');
-			}
-			if(line == 9)
-			{
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar(',');
-			}
-		}
 }
