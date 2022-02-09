7748e2e3b4e1cb3315323a41cc5f6db0a6cbff7e
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-09 07:41:56 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,18 +11,16 @@ void more_numbers(void)
 	int n = 0;
 	int i = 0;
 
-	for (i = 0; i <10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (n = 0; n <= 14; n++)
 		{
 			if (n >= 10)
 			{
-				_putchar(n / 10);
+				_putchar((n / 10) + '0');
 			}
-			_putchar(n % 10);
+			_putchar((n % 10) + '0');
 		}
 		_putchar('\n');
 	}
-
-	return (0);
 }
