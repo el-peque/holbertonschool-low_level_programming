44e4249bf8601f3c368b8187ddbb95e289e8e2be
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-09 11:42:53 -0800

    1-main.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,15 +7,15 @@
 
 void print_number(int n)
 {
-	if (n >= 1000)
+	if (n >= 1000 || n <= -1000)
 	{
 		_putchar((n / 1000) + '0');
 	}
-	if (n >= 100)
+	if (n >= 100 || n <= -100)
 	{
 		_putchar((n / 100) + '0');
 	}
-	if (n >= 10)
+	if (n >= 10 || n <= -10)
 	{
 		_putchar((n / 10) + '0');
 	}
