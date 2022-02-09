4c705357252092d85f19a90b52964ab35f96e20b
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-09 09:21:14 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,16 +12,21 @@ void print_triangle(int size)
 	int j = 0;
 	int k = 0;
 
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		for (k = 0; k <= (size - j); k++)
-		{
-			_putchar(' ');
-		}
-		for (j = 0; j <= i; j++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
 	}
+	else
+		for (i = 0; i < size; i++)
+		{
+			for (k = 0; k < (size - j - 1); k++)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 }
