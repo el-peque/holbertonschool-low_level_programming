2e1cbdc1288f2f8db27daaa5d1199b09b3e1ae72
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-11 10:50:14 -0800

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i])
-	{
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
-	}
+		i++;
 	}
 	_putchar ('\n');
 }
