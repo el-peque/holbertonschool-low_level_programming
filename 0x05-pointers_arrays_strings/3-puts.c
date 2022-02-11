a947aba5d82a46a660798312d7f83a84850d2137
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-11 08:04:19 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @*str: char *str
+ */
+
+int _puts(char *str)
+{
+	int i;
+
+	for(
+	_putchar(str[i]
+	long int n[i] = *s;
+
+	return(n);
+}
