2a43abcd2f307fe9bd355aa46ba141de2b3ddaff
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-15 06:02:08 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ *
+ * Return: not sure yet
+ */
+
+char *string_toupper(char *a)
+{
+	int n = 0;
+
+	while (a[n])
+	{
+		if (a[n] >= 97 && a[n]<=122)
+		{
+			a[n] -= 32;
+		}
+		n++;
+	}
+	return (a);
+}
