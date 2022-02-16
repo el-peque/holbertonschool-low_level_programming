bbe14785f5916d9c49420903c7302b56ee5dc3c5
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-16 05:27:41 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,16 +11,12 @@ void reverse_array(int *a, int n)
 {
 	int m = 0;
 	int k = 0;
-	int d = 0;
 
-	for (m = 0; a[m] != '\0' && m < n; m++)
+	for (m = 0; a[m] <= n; m++)
 	{}
-	m--;
 	k = m;
-	for (n = 0; n <= k; n++, m--)
+	for (n = 0; n < k; n++, m--)
 	{
-		d = a[m];
-		a[m] = a[n];
-		a[n] = d;
+		a[n] = a[m];
 	}
 }
