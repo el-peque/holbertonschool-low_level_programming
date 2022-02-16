e004904342c34692733fa118c0039750ca360222
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-16 06:04:50 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,6 +14,14 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
+	if (n >= 100000)
+	{
+		_putchar(((n / 100000) % 10) + '0');
+	}
+	if (n >= 10000)
+	{
+		_putchar(((n / 10000) % 10) + '0');
+	}
 	if (n >= 1000)
 	{
 		_putchar(((n / 1000) % 10) + '0');
