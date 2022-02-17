f3f7a42fc8501ca138921fec1dbcaf9ba86a157a
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-17 04:36:37 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,18 +9,19 @@
 
 void print_number(int n)
 {
-	int c = 0;
+	int c = 1;
 
-	for (c = 1; ; c *= 10)
+	if (n < 0)
 	{
-		if (n / c != 0)
-		{
-			_putchar(((n / c)  % 10) + '0');
-			continue;
-		}
-		else
-		{
-			break;
-		}
+		n = -n;
+		_putchar('-');
+	}
+	while (n / c != 0)
+	{
+		c *= 10;
+	}
+	for ( ; c >= 1; c /= 10)
+	{
+		_putchar(((n / c)  % 10) + '0');
 	}
 }
