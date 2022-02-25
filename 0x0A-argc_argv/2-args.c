55e9a2b2c64fa490d65d401047dbdb820c5fef73
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-25 05:12:25 -0800

    Update 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: argument count
+ * @argv: array of pointers
+ * Return: should be zero
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int n = 0;
+	int m = 0;
+	char *a = argv[n];
+
+	for (n = 0; n < argc; n++)
+	{
+		for (m = 0; a[m] != 0; m++)
+		{
+			putchar(a[m]);
+		}
+	}
+	return (0);
+}
