1e1f2faa30101e1c4a1c208cb7b9c8cac6344c23
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-02-25 04:54:26 -0800

    Update 0-whatsismyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * main - prints the program name
+ * @argc: argument count
+ * @argv: array of pointers
+ * Return: should be zero
  */
 
-int main(int argc __attribute__((unused)), char **argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	int n = 0;
+	char *a = argv[0];
 
-	for (n = 0; *argv[n] != '\0'; n++)
+	for (n = 0; a[n] != '\0'; n++)
 	{
-		_putchar(*argv[n]);
+		_putchar(a[n]);
 	}
+	_putchar('\n');
 	return (0);
 }
