51b736f5fe1b4a9bb1955a61d8034a31758bbb0c
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-02 12:16:04 -0800

    Added 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: ac
+ * @av: av
+ *
+ * Return: smthng
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char **grid;
+	int n = 0;
+
+	if (ac == 0 || **av == '\0')
+	{
+		return (NULL);
+	}
+	grid = malloc(sizeof(int *) * ac);
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (n = 0;  *av[n] != '\0'; n++)
+	{
+		grid[n] = malloc(sizeof(int) * *av[n]);
+		if (grid == NULL)
+		{
+			return (NULL);
+		}
+	}
+	return (*grid);
+}
