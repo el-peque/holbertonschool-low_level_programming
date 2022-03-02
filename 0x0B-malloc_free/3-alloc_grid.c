e985055dff9be2d7d833103991430ac776c7ef81
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-02 09:20:12 -0800

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	grid = malloc(sizeof(int*) * height);
+	grid = malloc(sizeof(int*) * width);
 	if (grid == NULL)
 	{
 		return (NULL);
