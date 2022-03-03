72bc339a953821ae60d97d53e748a4f9351d2e2c
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-03 11:57:27 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,7 +21,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1lngth++;
 	}
 	strconcat = malloc(sizeof(*s1) * s1lngth + sizeof(*s2) * n);
-	if (s1[0] == '\0')
+	if (s1 == NULL)
 	{
 		return (NULL);
 	}
