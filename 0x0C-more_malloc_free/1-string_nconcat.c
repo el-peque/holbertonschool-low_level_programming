308f156fc45dea2d7606602634a0748b6b9a17e4
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-03 12:20:00 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,6 +18,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int s1lngth = 0;
 	unsigned int s2lngth = 0;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	for (a = 0; s1[a] != '\0'; a++)
 	{
 		s1lngth++;
