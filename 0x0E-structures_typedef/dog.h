0fc5e8f8f8f4aee91248ae87dca47b0cb6ec8686
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-08 15:59:26 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - doggy
+ * @name: name of doggy
+ * @age:  age of doggy
+ * @owner: who owns doggy
+ *
+ * Description: dog defined by name, age and owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* DOG */
