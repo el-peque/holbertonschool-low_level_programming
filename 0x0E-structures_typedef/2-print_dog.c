83c540a0e390a97bea30efc3f8e8c63a43cab63f
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-08 16:58:11 -0800

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,28 +9,31 @@
 
 void print_dog(struct dog *d)
 {
-	char *name = (*d).name;
-	char *owner = (*d).owner;
-
 	if (d != NULL)
 	{
 		if ((*d).name == NULL)
 		{
-			name = "(nil)";
+			printf("Name: (nil)\n");
 		}
-		if ((*d).owner == NULL)
+		else
 		{
-			owner = "(nil)";
+			printf("Name: %s\n", (*d).name);
 		}
-		printf("Name: %s\n", name);
 		if ((*d).age < 0)
 		{
 			printf("Age: (nil)\n");
 		}
-		else if ((*d).age >= 0)
+		else
 		{
 			printf("Age: %f\n", (*d).age);
 		}
-		printf("Owner: %s\n", owner);
+		if ((*d).owner == NULL)
+		{
+			printf("Owner: (nil)\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", (*d).owner);
+		}
 	}
 }
