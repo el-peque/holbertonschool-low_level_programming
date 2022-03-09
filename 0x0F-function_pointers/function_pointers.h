22d3effaa1fb5e6ae4e1fa6bff11ebe2930a4491
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-09 11:21:05 -0800

    1-array_iterator.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
+#include <stddef.h>
 #ifndef FUNCPOINT
 #define FUNCPOINT
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCPOINT */
