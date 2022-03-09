ffea1dcb45d19e6ec77258497f04c198ee1773d8
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-09 11:29:53 -0800

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,8 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i = 0;
 
-	for (i = 0; i < size; i++)
+	if (array != NULL && size >= 1 && *action != NULL)
 	{
-		(*action)(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			(*action)(array[i]);
+		}
 	}
 }
