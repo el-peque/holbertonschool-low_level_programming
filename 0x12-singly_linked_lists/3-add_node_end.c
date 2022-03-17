cc77f0de3d9556e2ff1280dabd673bfc65265b6c
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-17 13:50:55 -0700

    3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: head of list
+ * @str: str
+ * Return: new head
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *nend;
+	list_t *naux = *head;
+
+	nend = malloc(sizeof(list_t));
+	if (nend == NULL)
+	{
+		return (NULL);
+	}
+	nend->str = strdup(str);
+	nend->len = strlen(str);
+	nend->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = nend;
+	}
+	else
+	{
+	while (naux->next != NULL)
+	{
+		naux = naux->next;
+	}
+	naux->next = nend;
+	}
+	return (nend);
+}
