2a24e3a98f20fbd8842901f434453f94fe1ce607
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-17 15:52:18 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * startfirst - prints a string
+ */
+
+void startfirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
