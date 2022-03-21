8faa0f9de08ce55f9fd61c6ad8f925ebe17380da
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-21 08:07:56 -0700

    Added task 1 and README

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list_tint list
+ * @h: list_tint h
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
