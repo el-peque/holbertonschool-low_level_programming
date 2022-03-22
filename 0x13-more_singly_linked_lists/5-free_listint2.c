10d4b48f9917eaed02d79002a43d3ad38b7f64d7
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-22 07:34:47 -0700

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,6 +9,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *aux;
 
+	if (head != NULL)
+	{
 	while ((*head)->next != NULL)
 	{
 		aux = (*head)->next;
@@ -17,4 +19,5 @@ void free_listint2(listint_t **head)
 	}
 	free(*head);
 	(*head) = NULL;
+	}
 }
