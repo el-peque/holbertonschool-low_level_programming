7a5053b088b95b4ef96cf6b2d0c1f020f872918c
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-22 07:30:13 -0700

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	nnode->n = n;
 	nnode->next = aux->next;
-	aux ->next = nnode;
+	aux->next = nnode;
 
 	return (nnode);
 }
