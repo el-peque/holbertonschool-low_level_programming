155532c657884baa1104f587bdd51466637225a7
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-22 06:53:10 -0700

    Fixed comments

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a listint_t list
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list
  * @head: head
+ * @index: index
+ * Return: node at index
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
