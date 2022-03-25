5a8fa81982d5fe3829f6d7e6ca662c8201c7dc14
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-24 18:57:51 -0700

    Added 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: unsigned long int
+ * @index: index
+ *
+ * Return: 1 if it worked, or -1 if and error occurred
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int newn;
+
+	newn = *n;
+	newn &= ~(1 << index);
+	*n = newn;
+	return (1);
+}
