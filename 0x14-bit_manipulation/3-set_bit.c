b7b38ff7a353a2c3ebda4ffc83976b22258afd9c
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-24 18:51:03 -0700

    Fixed 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,7 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int newn;
 
-	newn = 1 << index | *n;
-	n = &newn;
+	newn = (1 << index) | *n;
+	*n = newn;
 	return (1);
 }
