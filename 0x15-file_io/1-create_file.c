3ba3119e3c5e5fe67b808e55166414a056f7f8cf
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-28 16:23:13 -0700

    Added task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, size, b;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR, 00666);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		b = write(fd, "", 0);
+		if (b == -1)
+		{
+			return (-1);
+		}
+		return (1);
+	}
+
+	size = sizeof(text_content) + 1;
+
+	b = write(fd, text_content, size);
+	if (b == -1)
+	{
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
