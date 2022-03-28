7b1ae970560e38be3f249f55ab76720a95f657c6
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-28 13:57:29 -0700

    Fixed style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,18 +15,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
-		return 0;
+		return (0);
 	if (filename == NULL)
-		return 0;
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	read(fd, buff, letters);
 	buff[letters] = '\0';
-	close (fd);
+	close(fd);
 
 	fd = write(STDOUT_FILENO, buff, letters);
 	if (fd == -1)
-		return 0;
+		return (0);
 	close(fd);
 	size = strlen(buff) + 1;
 	free(buff);
