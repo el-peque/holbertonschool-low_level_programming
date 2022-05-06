bb1623d535f44555f04d9c6b22f1615d9a6e530f
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-06 15:25:06 -0700

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -22,6 +22,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if (*head == NULL)
 	{
+		nend->prev = NULL;
 		*head = nend;
 	}
 	else
@@ -31,6 +32,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 			aux = aux->next;
 		}
 		aux->next = nend;
+		nend->prev = aux;
 	}
 	return (nend);
 }
