57d9f277d28f0f06f75b5fd4b53caf6ce04bb36b
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-26 09:37:45 -0700

    Created 3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table you want to add or update the key/value to
+ * @key: key
+ * @value: value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int value = 0;
+
+	
+	value = hash_djb2(key);
+
+	return(ht)
+}
