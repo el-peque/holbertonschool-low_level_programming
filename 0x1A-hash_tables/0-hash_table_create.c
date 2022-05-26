f662878af9d55e4cca54d9d7b2fea9d02c25a178
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-26 09:06:53 -0700

    Created 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,27 @@
 #include "hash_tables.h"
 
 /**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
  *
- *
- *
+ * Return: newly created hash table or NULL if it failed
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht = NULL;
+
+	if (size <= 0)
+		return (NULL);
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(ht->array) * size);
+	if (ht->array == NULL)
+		return (NULL);
+
+	return (ht);
+}
