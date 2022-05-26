6b5d247b35ee0e87a6e0ca3ba54c67d8be59b8c9
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-26 09:07:11 -0700

    Created hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,9 @@
 #ifndef HASH_TABLES
 #define HASH_TABLES
 
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -11,9 +14,9 @@
  */
 typedef struct hash_node_s
 {
-  char *key;
-  char *value;
-  struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -26,8 +29,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-  unsigned long int size;
-  hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
