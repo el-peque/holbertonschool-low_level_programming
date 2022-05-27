05665a5b9c2e3b016a73065e171a9e2efdd9dd0d
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-27 07:09:27 -0700

    Created 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,27 +1,23 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: hash table you want to add or update the key/value to
- * @key: key
- * @value: value associated with the key
+ * hash_table_get -  retrieves a value associated with a key
+ * @ht: hash table you want to look into
+ * @key: key you are looking for
  *
- * Return: 1 if it succeeded, 0 otherwise
+ * Return:  value associated with the element, or NULL if key couldn’t be found
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int idx = 0;
-	hash_node_t *new_node;
 
-	new_node = malloc(sizeof(hash_node_t));
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
+	if (ht == NULL || key == NULL)
+		return (NULL);
 
 	idx = key_index((const unsigned char *)key, ht->size);
-	new_node->next = ht->array[idx]->next;
-	ht->array[idx] = malloc(sizeof(new_node));
-	ht->array[idx] = new_node;
+	if (idx > ht->size || ht->array[idx]->value == NULL || ht->array[idx] == NULL)
+		return (NULL);
 
-	return (1);
+	return (ht->array[idx]->value);
 }
