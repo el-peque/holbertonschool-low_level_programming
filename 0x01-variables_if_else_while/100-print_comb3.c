4b690a0e7029bc4c7129f83093fcd4df224207ac
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-06-24 21:38:24 -0700

    Removed unneeded files

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	int b = 1, i, j;
 
-	for(i = 0 ; i < 9 ; i++)
+	for (i = 0 ; i < 9 ; i++)
 	{
-		for(j = b; j <= 9; j++)
+		for (j = b; j <= 9; j++)
 		{
-			putchar (i + '0');
-			putchar (j + '0');
+			putchar(i + '0');
+			putchar(j + '0');
 			if (i < 8)
 			{
 				putchar(',');
