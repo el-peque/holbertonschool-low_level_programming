4dff283c27774d372d484425054ff33136b3d96f
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-07-14 10:59:49 -0700

    Create 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    p = 0
+    for r in range(len(grid)):
+        for h in range(len(grid[r])):
+            if grid[r][h] == 1:
+                if grid[r - 1][h] == 0:
+                    p += 1
+                if grid[r+1][h] == 0:
+                    p += 1
+                if grid[r][h-1] == 0:
+                    p += 1
+                if grid[r][h+1] == 0:
+                    p += 1
+    return(p)
