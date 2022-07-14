46f1749879139644d83e4e0250a62b8e19531111
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-07-14 11:25:03 -0700

    Fixed pycodestyle

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,11 +9,11 @@ def island_perimeter(grid):
         for h in range(len(grid[r])):
             if grid[r][h] == 1:
                 if grid[r-1][h] == 0 or r == 0:
-                        p += 1
+                    p += 1
                 if r == len(grid) - 1 or grid[r+1][h] == 0:
-                        p += 1
+                    p += 1
                 if grid[r][h-1] == 0 or h == 0:
-                        p += 1
+                    p += 1
                 if h == len(grid[r]) - 1 or grid[r][h+1] == 0:
-                        p += 1
+                    p += 1
     return(p)
