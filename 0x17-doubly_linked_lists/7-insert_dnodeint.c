a50a8ca794ad1afb5fadef8efbc8d9736cd55de8
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-07-22 15:37:30 -0700

    Fix 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,30 +14,53 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node;
 	dlistint_t *aux;
 
-	if (*h == NULL)
+	if (h == NULL)
 		return (NULL);
 
 	aux = *h;
+	if (idx == dlistint_len(aux))
+		return (add_dnodeint_end(h, n));
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
 	new_node->next = NULL;
-
-	for (i = 1; i < idx; i++)
-	{
-		if (aux->next == NULL)
-			return (NULL);
-		aux = aux->next;
-	}
-	if (aux->next != NULL)
-	{
-		new_node->next = aux->next;
-		aux->next->prev = new_node;
-	}
-	aux->next = new_node;
 	new_node->prev = aux;
 
+	for (i = 0; i < idx - 1; i++)
+	{
+		aux = aux->next;
+	}
+
+	if (aux->next == NULL)
+	{
+		free(new_node);
+		return (add_dnodeint_end(h, n));
+	}
+
+	new_node->next = aux->next;
+	aux->next->prev = new_node;
+	aux->next = new_node;
+
 	return (new_node);
+}
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list
+ * @h: dlist_tint h
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int nodes = 0;
+
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
 }
