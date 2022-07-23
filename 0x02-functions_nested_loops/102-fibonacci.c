a68134142e13742e8bc01c4280e2a2b1fa5c5f44
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-07-23 10:22:24 -0700

    Fixed 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,14 +8,18 @@
 
 int main(void)
 {
-	unsigned int a = 0, b = 0, f = 1, i = 0;
+	unsigned long int a = 0, b = 0, f = 1, i = 0;
 
-	for (i = 0; i < 50; i++)
+	for (i = 1; i < 50; i++)
 	{
 		a = b;
 		b = f;
 		f = a + b;
-		printf("%d, ", f);
+		printf("%ld, ", f);
 	}
+	a = b;
+	b = f;
+	f = a + b;
+	printf("%ld\n", f);
 	return (0);
 }
