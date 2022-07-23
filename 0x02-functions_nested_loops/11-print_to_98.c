2fd25492df5d1e9e04baade71fb9b1f636029ce9
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-07-23 09:22:53 -0700

    Create 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,42 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * times_table - displays a time table
- * input character
- *
- * Return: smthn
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: int n
  */
+
 void print_to_98(int n)
 {
-	int n = 0;
+	int i = n;
 
-	for (n = 0 ; n <= 9 ; n++)
+	while (i < 98)
 	{
-		res = (row * line);
-		if (line != 0)
-			{
-				_putchar(' ');
-				if (res < 10)
-				{
-					_putchar(' ');
-				}
-			}
-			if (res > 9)
-			{
-				_putchar((res / 10) + '0');
-				_putchar((res % 10) + '0');
-			}
-			else
-			{
-				_putchar(res + '0');
-			}
-			if (line == 9)
-			{
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar(',');
-			}
-		}
+		printf("%d, ", i);
+		i++;
+	}
+
+	while (i > 98)
+	{
+		printf("%d, ", i);
+		i--;
+	}
+	printf("%d\n", 98);
 }
