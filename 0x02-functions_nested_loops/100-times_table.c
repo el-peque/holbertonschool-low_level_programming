6cfbcc4bf5205a7fb2bb6d88c4c57618d9e49188
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-07-23 09:55:43 -0700

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,16 +8,26 @@
 
 void print_times_table(int n)
 {
-	int i = 0, j = 0;
+	int i = 0, j = 0, spaces = 9;
 
-	if (n > 15 || n == 0)
+	if (n > 15 || n < 0)
 		return;
 
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j < n; j++)
 		{
-			printf("%d, ", j * i);
+			printf("%d,", j * i);
+			if ((j + 1) * i == 0)
+				printf("   ");
+			else
+			{
+			spaces = 1000;
+			while (spaces > (j + 1) * i)
+			{
+				printf(" ");
+				spaces /= 10; }
+			}
 		}
 		printf("%d\n", j * i);
 	}
